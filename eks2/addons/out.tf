f5b4fb3d4cad8d097a8ca81200730e414e9c07a2
iyuva <79840779+iyuva@users.noreply.github.com>
2024-11-29 17:40:18 +0530

    Create out.tf

---
+++ eks2/addons/out.tf
@@ -0,0 +1,9 @@
+# modules/eks/outputs.tf
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "eks_addon_names" {
+  value = [for addon in aws_eks_addon.addon : addon.addon_name]
+}
