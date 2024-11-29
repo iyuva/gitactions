aa5a7e7f5b94544ddae8ab75a8c2bf486bdefa8a
iyuva <79840779+iyuva@users.noreply.github.com>
2024-11-29 17:47:50 +0530

    Update var.tf

--- eks2/addons/var.tf
+++ eks2/addons/var.tf
@@ -23,3 +23,75 @@ variable "addons" {
     config                     = map(string) # Optional add-on config
   }))
 }
+
+
+
+
+#####################
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+}
+
+variable "cluster_addons" {
+  description = "Map of EKS addons and their configurations"
+  type = map(object({
+    name                       = string
+    addon_version              = string
+    configuration_values       = map(string)
+    pod_identity_association   = list(object({
+      role_arn        = string
+      service_account = string
+    }))
+    preserve                  = bool
+    before_compute            = bool
+    resolve_conflicts_on_create = string
+    resolve_conflicts_on_update = string
+    service_account_role_arn   = string
+    timeouts                  = object({
+      create = string
+      update = string
+      delete = string
+    })
+    tags                       = map(string)
+  }))
+}
+
+variable "bootstrap_self_managed_addons" {
+  description = "Flag to control self-managed addon creation"
+  type        = bool
+  default     = true
+}
+
+variable "create" {
+  description = "Flag to indicate if resources should be created"
+  type        = bool
+  default     = true
+}
+
+variable "create_outposts_local_cluster" {
+  description = "Flag to indicate if resources should be created for outposts local cluster"
+  type        = bool
+  default     = false
+}
+
+variable "cluster_addons_timeouts" {
+  description = "Timeouts for cluster addons creation, update, and deletion"
+  type = object({
+    create = string
+    update = string
+    delete = string
+  })
+  default = {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
